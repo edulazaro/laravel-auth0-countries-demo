83f14ac34b3cf2e74e396187e49d9704e56a5c70
Edu Lazaro <edu@kenodo.com>
2024-04-08 09:16:46 +0200

    Test Inertia countries page

--- tests/Feature/CountriesPageTest.php
+++ tests/Feature/CountriesPageTest.php
@@ -6,6 +6,7 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Auth0\SDK\Configuration\SdkConfiguration;
 use Auth0\Laravel\Users\StatefulUser;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Inertia\Testing\AssertableInertia as Assert;
 use App\Models\User;
@@ -13,6 +14,25 @@ use App\Models\User;
 class CountriesPageTest extends TestCase
 {
     use RefreshDatabase;
+
+    /**
+     * Configure Auth user
+     */
+    private function setAuthUser()
+    {
+        $secret = uniqid();
+
+        config([
+            'auth0.AUTH0_CONFIG_VERSION' => 2,
+            'auth0.guards.default.strategy' => SdkConfiguration::STRATEGY_REGULAR,
+            'auth0.guards.default.domain' => uniqid() . '.auth0.com',
+            'auth0.guards.default.clientId' => uniqid(),
+            'auth0.guards.default.clientSecret' =>  $secret,
+            'auth0.guards.default.cookieSecret' => uniqid(),
+        ]);
+
+        return new StatefulUser(['sub' => uniqid('auth0|')]);
+    }
 
     /**
      * Test unauthenticated user redirect to Auth0 login page
@@ -33,21 +53,29 @@ class CountriesPageTest extends TestCase
     {
         $response = $this->get('/');
 
-        $secret = uniqid();
-
-        config([
-            'auth0.AUTH0_CONFIG_VERSION' => 2,
-            'auth0.guards.default.strategy' => SdkConfiguration::STRATEGY_REGULAR,
-            'auth0.guards.default.domain' => uniqid() . '.auth0.com',
-            'auth0.guards.default.clientId' => uniqid(),
-            'auth0.guards.default.clientSecret' =>  $secret,
-            'auth0.guards.default.cookieSecret' => uniqid(),
-        ]);
-
-        $user = new StatefulUser(['sub' => uniqid('auth0|')]);
-
+        $user = $this->setAuthUser();
         $response = $this->actingAs($user)->get(route('countries'));
 
         $response->assertStatus(200);
+    }
+
+        /**
+     * Test authenticated user access
+     */
+    public function test_can_view_countries(): void
+    {
+        Http::fake([
+            'https://restcountries.com/v3.1/all' => Http::response([
+                ['cca2' => 'ES', 'name' => ['common' => 'Spain'], 'flags' => ['png' => 'https://flagcdn.com/w320/es.png']],
+                ['cca2' => 'CY', 'name' => ['common' => 'Cyprus'], 'flags' => ['png' => 'https://flagcdn.com/w320/cy.png']],
+            ], 200),
+        ]);
+
+        $response = $this->get('/');
+
+        $user = $this->setAuthUser();
+        $response = $this->actingAs($user)->get('/countries');
+
+        $response->assertInertia(fn (Assert $page) => $page->has('countries', 2));
     }
 }
